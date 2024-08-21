805c6fa89569481f311f3d959fe3f7d092405c42
Erick Obregón <erick.obregon.2001@gmail.com>
2024-08-20 20:58:50 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+
+    //evento para hacer zoom en img
+    $('.img').on('mouseenter', function () {
+        $(this).addClass('zoom');
+    });
+
+    $('.img').on('mouseleave', function () {
+        $(this).removeClass('zoom');
+    });
+    
+    //evento para cambiar color del bk
+
+    const $form = $('form');
+ 
+    $form.on('mouseenter', function() {
+        $(this).css('background-color', '#f0f0f0');
+    });
+ 
+    $form.on('mouseleave', function() {
+        $(this).css('background-color', '');
+    });
+
+    //boton que muestre mensaje al completar form
+
+    $('.btn').on('click',function(){
+        alert('formulario enviado exitosamente!')
+    })
+
+})
